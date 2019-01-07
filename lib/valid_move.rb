1ef54fa8dabbfc67d4444e3e3cd65a655322805f
Anthony Hillen <ah205@students.uwf.edu>
2019-01-07 19:25:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,9 @@
-def valid_move?(board, index)
-  index.between?(0,8) && !(board.position_taken)
-end
-
 def position_taken?(board, index)
   board[index] == "X" || board[index] == "O"
 end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !(board.position_taken?)
+end
+
+
